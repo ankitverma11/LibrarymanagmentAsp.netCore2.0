d4315508f2ded7a803481e2694fc36d524a456e0
Ankit Verma <anki.verma02@hotmail.com>
2019-01-31 17:09:53 +0530

    Create New Lend Controller , LendViewModel and View Related to Lend Controoler

---
+++ libraryManagment/Controllers/LendController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagment.Data.Interfaces;
+using LibraryManagment.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagment.Controllers
+{
+    public class LendController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly ICustomerRepository _customerRepository;
+
+        public LendController(IBookRepository bookRepository , ICustomerRepository customerRepository)
+        {
+            _bookRepository = bookRepository;
+            _customerRepository = customerRepository;
+        }
+        [Route("Lend")]
+        public IActionResult List()
+        {
+            var availablebook = _bookRepository.FindWithAuthor(x => x.BorrownerId == 0);
+           if (availablebook.Count() == 0)
+            {
+                return View("Empty");
+            }
+           else
+            {
+                return View(availablebook);
+            }
+        }
+
+        public IActionResult LendBook(int bookID)
+        {
+            //Load CurrentBook and All Customer
+            //Send data to the lend view
+            var LendVm = new LendViewModel()
+            {
+                Book = _bookRepository.GetById(bookID),
+                Customers = _customerRepository.GetAll()
+               };
+
+            return View(LendVm);
+        }
+
+        [HttpPost]
+        public IActionResult LendBook(LendViewModel lendViewModel)
+        {
+            //update the database
+            var book = _bookRepository.GetById(lendViewModel.Book.BookId);
+            var customer = _customerRepository.GetById(lendViewModel.Book.BorrownerId);
+            book.Borrowner = customer;
+            _bookRepository.Update(book);
+
+            //redirect to ist view
+            return RedirectToAction("List");
+        }
+
+
+
+    }
+}
