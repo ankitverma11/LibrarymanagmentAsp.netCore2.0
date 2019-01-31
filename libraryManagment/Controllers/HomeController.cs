48eca42892459cdd54fa39d600d6e8cd972a517e
Ankit Verma <anki.verma02@hotmail.com>
2019-01-31 18:27:06 +0530

    Home Controller and library managment dashboard

--- libraryManagment/Controllers/HomeController.cs
+++ libraryManagment/Controllers/HomeController.cs
@@ -5,33 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LibraryManagment.Models;
+using LibraryManagment.Data.Interfaces;
+using LibraryManagment.ViewModel;
 
 namespace LibraryManagment.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IBookRepository _bookRepository;
+        private ICustomerRepository _customerRepository;
+        private readonly IAuthorRepository _authorRepository;
+
+        public HomeController(IBookRepository bookRepository , ICustomerRepository customerRepository , IAuthorRepository authorRepository)
+        {
+            _bookRepository = bookRepository;
+            _customerRepository = customerRepository;
+            _authorRepository = authorRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            var HomeVm = new HomeViewModel
+            {
+                AuthorCount = _authorRepository.Count(x => true),
+                CustomerCount = _customerRepository.Count(x => true),
+                BookCount = _bookRepository.Count(x => true),
+                LendBookCount = _bookRepository.Count(x => x.Borrowner != null)
+            };
+            return View(HomeVm);
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    
     }
 }
