7756b0ce4e7bbb7fa7a6a8ba56343b8cd05b6110
Ankit Verma <anki.verma02@hotmail.com>
2019-01-31 17:36:37 +0530

    Create new return Controller and View Controller to get all borrowed book from each customer and return then back to the library

---
+++ libraryManagment/Controllers/ReturnController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagment.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagment.Controllers
+{
+    //Controller get all borrowed book from each customer and return then back to the library so we need book and customer repository through the constructor
+    public class ReturnController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly ICustomerRepository _customerRepository;
+
+        public ReturnController (IBookRepository bookRepository  , ICustomerRepository customerRepository)
+        {
+            _bookRepository = bookRepository;
+            _customerRepository = customerRepository;
+        }
+
+        [Route("Return")]
+        public IActionResult List()
+        {
+            //Load all Borrowed books
+            var borrowebooks = _bookRepository.FindWithAuthorAndBorrowner(x => x.BorrownerId != 0);
+            if (borrowebooks == null || borrowebooks.Count() == 0)
+            {
+                return View("Empty");
+            }
+
+            return View(borrowebooks);
+        }
+
+        public IActionResult ReturnABook(int bookID)
+        {
+            //Load the Current book
+            var book = _bookRepository.GetById(bookID);
+            //remove borrower
+            book.Borrowner = null;
+            book.BorrownerId = 0;
+            //update database
+            _bookRepository.Update(book);
+            //redirect to list
+            return RedirectToAction("List");
+        }
+    }
+}
