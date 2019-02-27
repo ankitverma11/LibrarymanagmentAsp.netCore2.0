be94b62b7cc0a1e2a0deab59b675bcd2a7e5ef20
Ankit Verma <anki.verma02@hotmail.com>
2019-02-27 17:28:45 +0530

    Async and Await

---
+++ LandaexpressionActionFuncpredicate/AsyncAndAwait.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LandaexpressionActionFuncpredicate
+{
+    class AsyncAndAwait
+    {
+        //Async and Await are are the markers which mark code position from where control should resume after the task (thred) complete.
+        static void Main(string[] args)
+        {
+            Method();
+            Console.WriteLine("main thread");
+            Console.ReadLine();
+
+        }
+
+        public static async void Method()
+        {
+            await Task.Run(new Action(LongTask)); // main thred did not wait and long task execute after 20 second
+            Console.WriteLine("new Thread"); // wait untill the long task finished for 20 second
+        }
+
+        public static void LongTask()
+        {
+            Thread.Sleep(20000);
+        }
+    }
+}
