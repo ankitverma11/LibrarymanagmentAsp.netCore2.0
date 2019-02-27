ef82b61c1b06f52d3cd7ad2220941abdd7869864
Ankit Verma <anki.verma02@hotmail.com>
2019-02-27 17:32:00 +0530

    Extension methods

---
+++ LandaexpressionActionFuncpredicate/ExtensionMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LandaexpressionActionFuncpredicate
+{
+    class ExtensionMethod
+    {
+        //Extension Methods help you to add new method to the existing type without modifing  the orignal code , inheriting or aggregation.
+        static void Main (string[] args)
+        {
+            mathparty math = new mathparty();
+            math.Add(3,5);
+            math.substact(5, 3);
+        }
+    }
+
+   public class mathparty
+    {
+        public  int Add(int num1 , int num2)
+        {
+            return num1 + num2;
+        }
+    }
+    /// <summary>
+    /// Estension method
+    /// </summary>
+    public static class somemoremethod
+    {
+        public static int substact(this mathparty obj ,int num1,int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
